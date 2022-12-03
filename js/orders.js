2cb21def25b0ac82a2c156a08613d5dca331e815
Tanner Hart <tannerhart24@yahoo.com>
2022-12-02 17:05:56 -0800

    Created this template while following a tutorial of some concepts. Serves as a nice template to refer back to.

---
+++ js/orders.js
@@ -0,0 +1,39 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+   {
+    productName: 'Lavender Candles',
+    productNumber: '366666',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+   {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '49923',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+   {
+    productName: 'GPS 4K Drone',
+    productNumber: '933389',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+   {
+    productName: 'Drone',
+    productNumber: '355551',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+   {
+    productName: 'Laptop',
+    productNumber: '229933',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+
+];
